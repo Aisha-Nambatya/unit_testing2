423557a17015f7f60599834e87197e73c2fc5d86
Aisha Nambatya <marchbea42@gmail.com>
2024-10-22 21:54:51 +0300

    Added unit tests for Fibonacci

---
+++ test_fibonacci.py
@@ -0,0 +1,17 @@
+#pair: Aisha & Afrah
+import unittest
+
+from fibonacci import fibonacci_recursive
+
+def test_fibonacci_recursive():
+    #numbers i expect for the first 10 indices
+    expected_values = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+    for i in range(len(expected_values)):
+        result = fibonacci_recursive(i)
+        assert result == expected_values[i], f"Test failed for n={i}: expected {expected_values[i]}, got {result}"
+        print(f"Fibonacci({i}) = {result} (Test passed)")
+
+# Runing our tests
+if __name__== "_main_":
+    test_fibonacci_recursive()
